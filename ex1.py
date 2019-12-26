3496a64c30aca321229bea95f082ce899777b8d0
Ashish S <ashishjsharda@gmail.com>
2019-12-26 08:14:08 -0500

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,16 @@
+'''
+Created on Dec 26, 2019
+Using MatPlotLib
+@author: ashish
+'''
+from matplotlib import pyplot as plt
+import numpy as np
+import math
+x=np.arange(0,math.pi*2,0.05)
+y=np.sin(x)
+plt.plot(x,y)
+plt.xlabel("angle")
+plt.ylabel("sine")
+plt.title("sine wave")
+plt.show()
+
