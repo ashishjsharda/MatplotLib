b725c6ab226dccb897cca46302c11d98e56d5743
Ashish S <ashishjsharda@gmail.com>
2019-12-27 18:42:15 -0500

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -1,5 +1,12 @@
+'''
+Created on Dec 27, 2019
+
+@author: ashish
+'''
 import matplotlib.pyplot as plt
 appl_stock=[100,120,140,160]
 year=[2016,2017,2018,2019]
-plt.plot(appl_stock,year)
+plt.plot(year,appl_stock)
+plt.xlabel("Year")
+plt.ylabel("Stock Price")
 plt.show()
